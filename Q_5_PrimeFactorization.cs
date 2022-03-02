89ea18f63b6409b877cbd5d152754e464d61cfde
RISHABH <gguptarishabh82@gmail.com>
2022-03-03 03:40:35 +0530

    Print the prime factors of number N

---
+++ Q_5_PrimeFactorization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram_Day_5
+{
+    public class Q_5_PrimeFactorization
+    {
+        public static void Prime()
+        {
+            int a, b;
+            Console.WriteLine("Enter the Number : ");
+            a = int.Parse(Console.ReadLine());
+            for (b = 1; b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + " is factor of " + a);
+                }
+            }
+        }
+    }
+    }
