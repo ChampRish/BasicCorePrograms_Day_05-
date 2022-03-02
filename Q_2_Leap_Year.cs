1ccba87e2ca0b418a99022c1afa5c32d4ffc3f26
RISHABH <gguptarishabh82@gmail.com>
2022-03-03 02:48:38 +0530

    Calculate Leap Year

---
+++ Q_2_Leap_Year.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram_Day_5
+{
+    public class Q_2_Leap_Year
+    {
+        public static void LeapYear()        
+        {
+            Console.Write("Enter your Year: ");
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0)
+                Console.WriteLine("This year is leap year");
+            else
+                Console.WriteLine("This year is not leap year");
+        }
+    }
+}
