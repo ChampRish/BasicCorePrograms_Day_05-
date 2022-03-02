1f6bd7baa912f8a64509ebe08aecbfff368f6b33
RISHABH <gguptarishabh82@gmail.com>
2022-03-03 03:07:46 +0530

    Calculate Power of Two

---
+++ Q_3_PowerOf_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram_Day_5
+{
+    public class Q_3_PowerOf_2
+    {
+        public static void powerTwo()
+        {
+            Console.WriteLine("Enter the value of N : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            {
+                double pow_t = Math.Pow( 2, n);
+                Console.WriteLine(pow_t);
+                if ((pow_t % 4 == 0))
+                {
+                    Console.WriteLine("This Year is a Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine("This Year is Not a Leap Year");
+                }
+            }
+        }
+    }
+}
