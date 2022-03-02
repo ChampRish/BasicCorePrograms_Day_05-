8cc6ed92f223183638ee0f51cf9526e52ca59d82
RISHABH <gguptarishabh82@gmail.com>
2022-03-03 03:58:11 +0530

    Program to Check Whether a Number is Even or Odd

---
+++ Q_8_CheckNumberEvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram_Day_5
+{
+    public class Q_8_CheckNumberEvenOrOdd
+    {
+        public static void OddEven()
+        {
+            int num1, rem1;
+            Console.Write("\n\n");
+            Console.Write("Check whether a number is even or odd :\n");  // \n is used for new line
+            Console.Write("---------------------------------------");
+            Console.Write("\n\n");
+            Console.Write("Input an integer : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            rem1 = num1 % 2;
+            if (rem1 == 0)
+                Console.WriteLine("{0} is an even integer.\n", num1);                // placeholder calling
+            else
+                Console.WriteLine("{0} is an odd integer.\n", num1);
+        }
+    }
+}
